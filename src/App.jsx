2b8fe98ffa14e9757475f9da776e35cf57059370
Stanley J Delva <stanleyjdelva@Stanleys-MacBook-Pro.local>
2025-06-10 14:56:44 -0400

    needed components are set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <StepNumbers />
+        <StepWriting />
+        <NavigationBtns />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function StepNumbers() {
+  return (
+    <div className="step-numbers">
+      <button className="step-1">1</button>
+      <button className="step-2">2</button>
+      <button className="step-3">3</button>
+    </div>
+  );
+}
+
+function StepWriting() {
+  return (
+    <div className="step-p">
+      <p>Learn React</p>
+    </div>
+  );
+}
+
+function NavigationBtns() {
+  return (
+    <div className="navigation-btns">
+      <button className="prev-btn">Previous</button>
+      <button className="next-btn">Next</button>
+    </div>
+  );
+}
+
+export default App;
