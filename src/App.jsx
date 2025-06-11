bb153610f3d2b586074a872c5fb032043e94c184
Stanley Delva <juniordelva1@gmail.com>
2025-06-11 18:01:38 -0400

    added the close btn and some css styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,45 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="main-container">
-        <StepNumbers />
-        <StepWriting />
-        <NavigationBtns />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <button className="close-btn">✖</button>
+            <div className="step-container">
+                <StepNumbers />
+                <StepWriting />
+                <NavigationBtns />
+            </div>
+        </>
+    );
 }
 
 function StepNumbers() {
-  return (
-    <div className="step-numbers">
-      <button className="step-1">1</button>
-      <button className="step-2">2</button>
-      <button className="step-3">3</button>
-    </div>
-  );
+    return (
+        <div className="step-numbers">
+            <button className="step-btns">1</button>
+            <button className="step-btns">2</button>
+            <button className="step-btns">3</button>
+        </div>
+    );
 }
 
 function StepWriting() {
-  return (
-    <div className="step-p">
-      <p>Learn React</p>
-    </div>
-  );
+    return (
+        <div className="step-p">
+            <p>Learn React</p>
+        </div>
+    );
 }
 
 function NavigationBtns() {
-  return (
-    <div className="navigation-btns">
-      <button className="prev-btn">Previous</button>
-      <button className="next-btn">Next</button>
-    </div>
-  );
+    return (
+        <div className="navigation-btns">
+            <button className="prev-btn">Previous</button>
+            <button className="next-btn">Next</button>
+        </div>
+    );
 }
 
 export default App;
